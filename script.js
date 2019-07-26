18944d250d44d60ca9e5a11c905a56d785367bab
celyes <celyes02@gmail.com>
2019-07-26 03:55:20 +0100

    update files - index.html script.js

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-var countDown = new Date("Jul 27, 2019 10:00:00").getTime();
+/* var countDown = new Date("Jul 27, 2019 10:00:00").getTime();
 var x = setInterval(function(){
-  var now = new Date().getTime(),
-  distance = countDown - now,
+  var now;
+  var getTime= new XMLHttpRequest();
+  getTime.onreaedystatechage = function(){
+    if(this.readyState == 4 && this.status == 200){
+
+      now = new Date(JSON.parse(this.responseText).unixtime).getTime();
+      console.log(now)
+    }
+  }
+  getTime.open("GET", "http://worldtimeapi.org/api/timezone/Africa/Algiers", true);
+  getTime.send();
+
+
+  var distance = countDown - now,
   days = Math.floor(distance / (1000 * 60 * 60 * 24)),
   hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
   minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
   seconds = Math.floor((distance % (1000 * 60)) / 1000),
   timeString  = days + " day :: ";
+
   timeString += hours + " hours :: ";
-  timeString += minutes + " minutes :: "; 
+  timeString += minutes + " minutes :: ";
   timeString += seconds + " seconds";
-  console.log(timeString);
   document.querySelector("#count-down").innerHTML = timeString;
   if(distance < 0){
     clearInterval(x);
@@ -18,3 +30,4 @@ var x = setInterval(function(){
   }
 
 }, 1000);
+*/
